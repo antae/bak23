fcc497b93104b24859355e86f34ba33823295fdc
antae <bukauskas.antanas@gmail.com>
2023-05-13 11:54:28 +0300

    initial commit

--- src/unet.py
+++ src/unet.py
@@ -2,16 +2,17 @@ import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 import tensorflow as tf
-from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation, MaxPool2D, Conv2DTranspose, UpSampling2D, Concatenate, Input
+from tensorflow.keras.layers import Conv2D, Activation, MaxPool2D, Conv2DTranspose, UpSampling2D, Concatenate, Input, multiply
 from tensorflow.keras.models import Model
+from tensorflow.python.keras import backend as K
 
 def conv_block(inputs, num_filters):
     x = Conv2D(num_filters, 3, padding="same")(inputs)
-    x = BatchNormalization()(x)
+    #x = BatchNormalization()(x)
     x = Activation("relu")(x)
 
     x = Conv2D(num_filters, 3, padding="same")(x)
-    x = BatchNormalization()(x)
+    #x = BatchNormalization()(x)
     x = Activation("relu")(x)
 
     return x
@@ -22,10 +23,62 @@ def encoder_block(inputs, num_filters):
     return x, p
 
 def decoder_block(inputs, skip, num_filters):
-    x = Conv2DTranspose(num_filters, (2, 2), strides=2, padding="same")(inputs)
-    x = UpSampling2D(size=(2, 2), interpolation='nearest')(inputs)
-    x = Concatenate()([x, skip])
+    #x = Conv2DTranspose(num_filters, (2, 2), strides=2, padding="same")(inputs)
+    g = gating_signal(inputs, num_filters, True)
+    att = attention_block(skip, g, num_filters)
+    up = UpSampling2D(size=(2, 2), interpolation='nearest')(inputs)
+    x = Concatenate()([up, att])
     x = conv_block(x, num_filters)
+    return x
+
+def repeat_elem(tensor, rep):
+    # lambda function to repeat Repeats the elements of a tensor along an axis
+    #by a factor of rep.
+    # If tensor has shape (None, 256,256,3), lambda will return a tensor of shape 
+    #(None, 256,256,6), if specified axis=3 and rep=2.
+
+    return tf.keras.layers.Lambda(lambda x, repnum: tf.keras.backend.repeat_elements(x, repnum, axis=3),
+                          arguments={'repnum': rep})(tensor)
+
+def attention_block(x, gating, inter_shape):
+    shape_x = K.int_shape(x)
+    shape_g = K.int_shape(gating)
+
+# Getting the x signal to the same shape as the gating signal
+    theta_x = Conv2D(inter_shape, (2, 2), strides=(2, 2), padding='same')(x)  # 16
+    shape_theta_x = K.int_shape(theta_x)
+
+# Getting the gating signal to the same number of filters as the inter_shape
+    phi_g = Conv2D(inter_shape, (1, 1), padding='same')(gating)
+    upsample_g = Conv2DTranspose(inter_shape, (3, 3),
+                                 strides=(shape_theta_x[1] // shape_g[1], shape_theta_x[2] // shape_g[2]),
+                                 padding='same')(phi_g)  # 16
+
+    concat_xg = Concatenate()([upsample_g, theta_x])
+    act_xg = Activation('relu')(concat_xg)
+    psi = Conv2D(1, (1, 1), padding='same')(act_xg)
+    sigmoid_xg = Activation('sigmoid')(psi)
+    shape_sigmoid = K.int_shape(sigmoid_xg)
+    upsample_psi = UpSampling2D(size=(shape_x[1] // shape_sigmoid[1], shape_x[2] // shape_sigmoid[2]))(sigmoid_xg)  # 32
+
+    upsample_psi = repeat_elem(upsample_psi, shape_x[3])
+
+    y = multiply([upsample_psi, x])
+
+    result = Conv2D(shape_x[3], (1, 1), padding='same')(y)
+    #result = BatchNormalization()(result)
+    return result
+
+def gating_signal(input, out_size, batch_norm=False):
+    """
+    resize the down layer feature map into the same dimension as the up layer feature map
+    using 1x1 conv
+    :return: the gating feature map with the same dimension of the up layer feature map
+    """
+    x = Conv2D(out_size, (1, 1), padding='same')(input)
+    # if batch_norm:
+    #     x = BatchNormalization()(x)
+    x = Activation('relu')(x)
     return x
 
 def build_unet(input_shape, num_classes):
